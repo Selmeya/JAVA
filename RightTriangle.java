4821ba502e163fa2d665d8a27e3a5917293cc849
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-12 14:09:17 +0530

    Update RightTriangle.java

--- RightTriangle.java
+++ RightTriangle.java
@@ -3,8 +3,7 @@ class RightTriangle{
    public static void main(String[]args){
      System.out.println("n =");
      Scanner sc = new Scanner(System.in);
-     int rows
- = sc.nextInt();
+     int rows = sc.nextInt();
      for ( int i = 1; i<= rows;i++){
            for (int j = 1; j<= i ;j++){
                System.out.print("  *");
