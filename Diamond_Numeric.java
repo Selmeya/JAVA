c32d4b091410639ab0f9c7ab3fa5860e408b82ec
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-13 10:23:00 +0530

    Add files via upload

---
+++ Diamond_Numeric.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Diamond_Numeric{
+  public static void main(String[] args)  {  
+   int i, j, rows, num; 
+   System.out.println("Enter the number to be printed: ");
+   Scanner s = new Scanner(System.in);
+   num = s.nextInt();
+   Scanner sc = new Scanner(System.in);  
+   System.out.print("Max number of time printed ");  
+   rows = sc.nextInt();          
+   for (i= 0; i<= rows-1; i++)  {  
+     for (j=0; j<=i; j++)   {  
+        System.out.print(num);  
+     }   
+     System.out.println("");   
+   }   
+   for (i=rows-1; i>=0; i--){  
+      for(j=0; j <= i-1;j++){  j
+           System.out.print(num);  
+      }  
+      System.out.println("");  
+   }  
+ }  
+} 
