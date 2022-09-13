23d9e41676475e1d4519a365654c9b8424ffa1b8
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-13 11:52:55 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Square
+{
+ public static void main(String[]args)
+ {
+   int N,N2,a;
+   System.out.print("enter the number:");
+   Scanner as=new Scanner(System.in);
+   N=as.nextInt();
+   N2=N*N;
+   System.out.print("the square of the enterd numberis"+N2);
+  }
+}
