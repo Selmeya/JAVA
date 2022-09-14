0e86d56536bc8754a9202b17de07692f37556844
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-14 22:18:52 +0530

    Add files via upload

--- ATM_Notes.java
+++ ATM_Notes.java
@@ -4,42 +4,25 @@ public class ATM_Notes
 {
     public static void main(String args[])
     {
-        int amount, i2000=0, i500=0, i200=0, i100=0, count=0; 
-        Scanner s = new Scanner(System.in);
- 
-        System.out.print("Enter The Amount in Rupees : \n\n");
-        amount = s.nextInt();
- 
-    while(amount >= 2000) 
- { 
-  i2000 = amount / 2000 ; 
-  amount = amount % 2000;
-  System.out.print("\nTotal Number Of 2000 Rupees Notes :"+ i2000) ;
-  break ; 
- }
- while(amount >= 500) 
- { 
-  i500 = amount / 500 ; 
-  amount = amount % 500;
-  System.out.print("\nTotal Number Of 500 Rupees Notes : "+ i500) ;
-  break ; 
- } 
- while(amount >= 200) 
- { 
-  i200 = amount / 200 ; 
-  amount = amount % 200;
-  System.out.print("\nTotal Number Of 200 Rupees Notes : "+ i200) ;
-  break ; 
- } 
- while(amount >= 100) 
- { 
-  i100 = amount / 100 ;
-  amount = amount % 100; 
-  System.out.print("\nTotal Number Of 100 Rupees Notes : "+ i100) ; 
-  break ; 
- }  
-
- count = i2000 + i500 + i200 + i100;   
- System.out.print("\n\nTotal Number Of Notes Require : \n\n"+ count) ; 
- }
+       Scanner s=new Scanner(System.in);
+       int A,A1,B,B1,C,C1,D,D1,X;
+       System.out.println("Enter the 1st Denomination:");
+       A=s.nextInt();
+       System.out.println("Enter the 1st Denomination number of notes");
+       A1=s.nextInt();
+       System.out.println("Enter the 2nd Denomination:");
+       B=s.nextInt();
+       System.out.println("Enter the 2nd Denomination number of notes");
+       B1=s.nextInt();
+       System.out.println("Enter the 3rd Denomination:");
+       C=s.nextInt();
+       System.out.println("Enter the 3rd Denomination number of notes");
+       C1=s.nextInt();
+       System.out.println("Enter the 4th Denomination:");
+       D=s.nextInt();
+       System.out.println("Enter the 4th Denomination number of notes");
+       D1=s.nextInt();
+       X=((A*A1)+(B*B1)+(C*C1)+(D*D1));
+       System.out.println("Total Available Balance in ATM:"+X);
+     }
 }
