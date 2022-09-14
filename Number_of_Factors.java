63477e51d146feddfc4a344c8c1b598f38e01310
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-09-14 10:31:27 +0530

    Add files via upload

--- Number_of_Factors.java
+++ Number_of_Factors.java
@@ -4,9 +4,10 @@ public class Number_of_Factors {
   public static void main(String[] args) {
     int number;
     int count = 0;
+    System.out.print("Given Number: ");
     Scanner sc = new Scanner (System.in);
     number= sc.nextInt();
-    System.out.print("Given Number: ");
+   
 
     System.out.print("Number of Factors of " + number + " are: ");
     for (int i = 1; i <= number; ++i) {
