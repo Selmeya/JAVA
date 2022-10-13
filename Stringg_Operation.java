91e9ae71cc310bc86aebc04811c995460148bcd7
Selmeya <112368898+Selmeya@users.noreply.github.com>
2022-10-13 10:11:35 +0530

    Add files via upload

--- Stringg_Operation.java
+++ Stringg_Operation.java
@@ -38,6 +38,7 @@ public class Stringg_Operation{
    }
    static void Vowel(String s){
      System.out.println("Vowels : ");
+
      for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'
                     || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U') {
@@ -48,12 +49,14 @@ public class Stringg_Operation{
       }
     }
     static void Consonant(String s){
-     System.out.println("Consonant : ");
+     System.out.println("Consonants: ");
      for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'
-                    || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U') {
-             }
-             else{
+            if (s.charAt(i) == 'b' || s.charAt(i) == 'c' || s.charAt(i) == 'd' || s.charAt(i) == 'f' || s.charAt(i) == 'g'
+                    || s.charAt(i) == 'h' || s.charAt(i) == 'j' || s.charAt(i) == 'k' || s.charAt(i) == 'l' || s.charAt(i) == 'm'|| s.charAt(i) == 'n' || s.charAt(i) == 'p' || s.charAt(i) == 'q' || s.charAt(i) == 'r'
+                    || s.charAt(i) == 's' || s.charAt(i) == 't' || s.charAt(i) == 'v' || s.charAt(i) == 'w' || s.charAt(i) == 'x'|| s.charAt(i) == 'y' || s.charAt(i) == 'z' || s.charAt(i) == 'B' || s.charAt(i) == 'C'
+                    || s.charAt(i) == 'D' || s.charAt(i) == 'F' || s.charAt(i) == 'G' || s.charAt(i) == 'H' || s.charAt(i) == 'J'|| s.charAt(i) == 'K' || s.charAt(i) == 'L' || s.charAt(i) == 'M'|| s.charAt(i) == 'N' || s.charAt(i) == 'P' || s.charAt(i) == 'Q' || s.charAt(i) == 'R'
+                    || s.charAt(i) == 'S' || s.charAt(i) == 'T' || s.charAt(i) == 'V' || s.charAt(i) == 'W' || s.charAt(i) == 'X'|| s.charAt(i) == 'Y' || s.charAt(i) == 'Z' ) {
+             
                 System.out.println( s.charAt(i) );
              
               }
